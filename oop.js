4ddb25dec73a538623972f448432d5e9396adcc7
Gus Deribeaux <108699203+Gderibeaux@users.noreply.github.com>
2022-07-19 17:52:16 -0400

    Update oop.js

--- oop.js
+++ oop.js
@@ -3,7 +3,21 @@
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn {
+  constructor(name){
+  this.name = name;
+  this.color = "silver"; 
+  this.sentence = "Hi";
+  }
+  changeSentence(newSentence){
+    this.sentence = newSentence;
+  }
+}
+var horse1 = new Unicorn("Friday");
+console.log(horse1);
 
+horse1.changeSentence(`*~* ${horse1.sentence} *~*`);
+  console.log(horse1);  
 
 // Write a class called Vampire
 // it should have a dynamic name property
@@ -11,7 +25,21 @@
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
 
+class Vampire {
+  constructor(name){
+    this.name = name;
+    this.animal = "Bat";
+    this.thirst = "True";
+  }   
+  changeThirst(newThirst){
+    this.thirst = newThirst;
+  }
+}
+  var bat1 = new Vampire("Dracula");
+ console.log(bat1);
 
+bat1.changeThirst("False");
+console.log(bat1);
 
 //  Write a Dragon class
 // it should have a dynamic name property (string)
@@ -21,7 +49,26 @@
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
 
+class Dragon{
+  constructor(name, rider, color){
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.isHungry = "True";
+  }
 
+  changeIsHungry(newisHungry){
+    this.isHungry = newisHungry;
+  }
+  
+}
+var eats = 4;
+var dragon1 = new Dragon("Henry", "Jack", "Blue");
+console.log(dragon1);
+if (eats === 4){
+ dragon1.changeIsHungry("False");
+console.log(dragon1); 
+}
 
 //  BIG CHALLNEGE: Write a Hobbit class
 //  it should have a dynamic name property (string)
@@ -31,3 +78,34 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit{
+  constructor(name, disposition){
+    this.name = name;
+    this.disposition = disposition;
+    this.age = 0;
+    this.isAdult = "False";
+    this.isOld = "False";
+  }
+  changeAge(newAge){
+    this.age = newAge;
+  }
+  changeisAdult(newIsAdult){
+    this.isAdult = newIsAdult;
+  }
+  changeIsOld(newIsOld){
+    this.isOld = newIsOld;
+  }
+}
+var hobbitsAge = 50
+var hobbit1 = new Hobbit("Gary", "Happy");
+console.log(hobbit1);
+if (hobbitsAge >= 33 && hobbitsAge <= 100){
+  hobbit1.changeisAdult("True")
+} else if (hobbitsAge >= 101) {
+  hobbit1.changeIsOld("True"); 
+  console.log(hobbit1);
+}
+
+  
+hobbit1.changeAge(hobbit1.age + 1);
+console.log(hobbit1);
